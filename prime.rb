a9197c5da5e65181f3218e4ea76cf4c1a875d40c
Emily Asch <e.asch@me.com>
2018-10-10 19:37:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 def prime?(num)
 return false if number <= 1 
-Math.sqrt(number).to_i.downto(2).each {|i| return false if }
+Math.sqrt(number).to_i.downto(2).each {|i| return false if number % i == 0}
+true
 end
